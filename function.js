46e62e2957ba5c3cde6977c43ff526d7cfa425a4
cameron Rimon <rimon072159@gmail.com>
2026-01-31 05:17:57 -0800

    What are function and function syntax

---
+++ function.js
@@ -0,0 +1,18 @@
+
+const money = 50;
+
+function name() {
+    console.log("I am a Rimon");
+    console.log("I am a Student");
+    console.log("click the switch to off");
+
+}
+
+function add() {
+     console.log("pink yp the ball");
+     console.log("add paste");
+     console.log("cut the paper");
+
+}
+
+name();
