1bc61ac280fb802f6a4da4dd37fd97e5c964c8e6
cameron Rimon <rimon072159@gmail.com>
2026-01-31 20:32:44 -0800

    Function return

--- multiple.js
+++ multiple.js
@@ -23,3 +23,14 @@ function add(num1, num2) {
 }
 
 add (10, 20);
+
+function difference(num1, num2) {
+   const diff = num1 - num2;
+   console.log(num1, num2, 'difference is', diff)
+
+}
+
+const father = 40;
+const myAge = 10;
+
+difference(father, myAge);
---
+++ return.js
@@ -0,0 +1,9 @@
+function tenTimes(number) {
+    const result = number * 10;
+    return result;
+}
+
+tenTimes(5);
+
+const output = tenTimes(4);
+console.log(output);
