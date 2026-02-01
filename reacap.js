5492881eac80be92e8229186f3d67f18e6a0ffbb
cameron Rimon <rimon072159@gmail.com>
2026-01-31 21:40:29 -0800

    reacap.js function

---
+++ reacap.js
@@ -0,0 +1,20 @@
+function add(price1, price2) {
+    const sum = price1 + price2;
+    return sum;
+}
+
+const num = add(10, 50);
+console.log(num);
+
+
+function multiply(price1) {
+    if (price1 % 2 === 0){
+        return true;
+    }else {
+        return false;
+    }
+
+}
+
+console.log(multiply(8));
+console.log(multiply(110));
