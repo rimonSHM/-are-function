c0c7f05e9da055e87d5fc8e08b767cb3cb00e35e
cameron Rimon <rimon072159@gmail.com>
2026-01-31 19:22:07 -0800

    function number

---
+++ multiple.js
@@ -0,0 +1,25 @@
+// function vaatKhao() {
+//     console.log("hand wash kore aso");
+//     console.log("boso");
+//     console.log("plate e khabar dhabar nao");
+//     console.log("gopat gopat kore khao");
+// }
+
+// vaatKhao();
+
+// function square(num) {
+//     console.log(num);
+//     const resulet = num * num;
+//     console.log(resulet)
+
+// }
+
+// square (3);
+
+
+function add(num1, num2) {
+   const sum = num1 + num2;
+   console.log(sum)
+}
+
+add (10, 20);
